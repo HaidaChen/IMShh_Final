420f992f7328ce802662e03edc07c4a9dee3866b
HaidaChen <mimi135@21cn.com>
2018-11-28 16:42:48 +0800

    common.js修改成app.js，同时增加页面的icon

---
+++ src/main/webapp/js/app.js
@@ -0,0 +1,253 @@
+var App = function () {
+	
+	var nthTabs;
+	var handleSidebar = function(current){
+		
+		$("#siderbar-warper").slimScroll({
+			height: "100%",
+			color: '#54667a'
+		});
+		var menuContainer = $("#side-menu");		
+		$.ajax({url:getProjectName()+"/login/getUserMemu.do",
+			success:function(menus){
+				$.each(menus, function(index, obj) {
+					var item = $("<li>");
+					var itemLink = $("<a href='javascript:;' class=''></a>");
+					var icon = obj.icon;
+					if (icon && icon != '' && icon.indexOf('+') > -1){
+						icon = icon.replace(/\+/g, ' ');
+					}
+					var itemContent = $("<i class='" + icon + "'></i> <span>" + obj.name +"</span>");
+					if (obj.url && obj.url != ''){
+						itemLink.attr("link", obj.url);
+						itemLink.attr("identify", obj.id);
+						itemLink.attr("text", obj.name);
+					}
+					itemLink.append(itemContent);
+					item.append(itemLink);
+					menuContainer.append(item);
+					if (obj.submenu && obj.submenu.length > 0){
+						var subcontainer = $("<ul class='nav-list'>");
+						$.each(obj.submenu, function(index, submenu){
+							var subItem = $("<li><a class='' href='javascript:;' link='"+submenu.url+"' identify='"+submenu.id+"' text='"+obj.name+"."+submenu.name+"'>" + submenu.name + "</a></li>");
+							subcontainer.append(subItem);
+						});
+						item.append(subcontainer);
+						itemLink.append("<span class='arrow'></span>");
+						
+					}else{
+						itemLink.attr("href", obj.url);
+					}				
+				});
+				
+			}
+		});
+	}
+
+	var handleSidebarCollapse = function(){
+		var menuContainer = $("#side-menu");
+		menuContainer.on("click", "li", function(){
+			var child = $(this).find("ul");
+			
+			var arrow = $(this).find("span.arrow");
+			child.toggle();
+			arrow.toggleClass("open");
+			menuContainer.find("span.arrow").not(arrow).removeClass("open");
+			menuContainer.find("ul").not(child).hide();
+			
+			var link = $(this).children("a").attr("link");
+			if(link && link != ''){
+				var identify = $(this).children("a").attr("identify");
+				var text = $(this).children("a").attr("text");
+				nthTabs.addTab({id:identify,title:text,content:link});
+			}
+		});
+		
+	}
+	
+	var handleMoreCondition = function(){
+		if ($("#nav_more_condition")){
+			$("#nav_more_condition").click(function(){
+				$("#panel_more_condition").toggleClass("open");
+			});			
+		}
+	}
+	
+	var handleChangePWD = function(){
+		$("#nav_changePWD").click(function(){
+			var html = ''+
+			'<form id="pwdForm" class="form-horizontal">' +
+               '<div class="form-group">' +
+                   '<label for="oldPassword" class="col-sm-4 control-label">当前密码：' + '</label>' +
+                   '<div class="col-sm-8">' + 
+                   	   '<input type="password" class="form-control" id="oldPassword" name="opassword" >' +
+                   '</div>' +
+               '</div>' +                
+               '<div class="form-group">' +
+                   '<label for="newPassword" class="col-sm-4 control-label">新的密码：' + '</label>' +
+                   '<div class="col-sm-8">' + 
+                   	   '<input type="password" class="form-control" id="newPassword" name="npassword">' +
+                   '</div>' +
+               '</div>' +               
+               '<div class="form-group">' +
+                   '<label for="repPassword" class="col-sm-4 control-label">密码确认：' + '</label>' +
+                   '<div class="col-sm-8">' + 
+                   	   '<input type="password" class="form-control" id="repPassword" name="rpassword">' +
+                   '</div>' +
+               '</div>' +
+           '</form>' +
+           '<div id="success_changePWD" class="alert alert-success" role="alert" style="display: none">密码修改成功</div>' +
+           '<div id="warn_changePWD" class="alert alert-warning" role="alert" style="display: none"></div>';
+			
+			Ewin.confirm({message: html, closeAuto: false}).on(function(e){
+				if (!e){
+					return;
+				}
+				
+				$("#pwdForm").bootstrapValidator({
+					fields: {
+			        	opassword : {validators: {notEmpty : {}}},
+			        	npassword : {validators: {notEmpty : {}, identical: {field: 'rpassword'}}},
+			        	rpassword : {validators: {identical: {field: 'npassword'}}}
+			        }
+				});
+				
+				var bootstrapValidator = $("#pwdForm").data('bootstrapValidator');
+				bootstrapValidator.validate();
+				if(!bootstrapValidator.isValid()){
+					return;
+				}	
+				$("#pwdForm").ajaxSubmit({
+					type: "post",
+					url:getProjectName()+"/login/changePWD.do", 
+					success:function(result){
+						if (result == -1){
+							$("#warn_changePWD").html("密码修改失败！原因：当前密码错误");
+							$("#warn_changePWD").show();
+							$("#success_changePWD").hide();
+						}
+						if (result == 1){
+							$("#warn_changePWD").html("");
+							$("#warn_changePWD").hide();
+							$("#success_changePWD").show();		
+						}
+					},
+					error:function(){
+						window.location.href = getProjectName() + "/login.html";
+					}
+					
+				});
+				
+				return false;
+			});
+		});
+	}
+	
+	var handleSignOut = function(){
+		$("#nav_signOut").click(function(){
+			Ewin.confirm({message: "确定要退出系统吗？"}).on(function(e){
+				if (!e){
+					return;
+				}
+				
+				$.ajax({url: getProjectName()+"/login/logout.do", success: function(){
+					window.location.href = getProjectName() + "/login.html";
+				}});
+			});
+		})
+	}
+	
+	
+	/*-----------------------------------------------------------------------------------*/
+	/*	处理登录模块
+	/*-----------------------------------------------------------------------------------*/	
+	var initLoginModule = function(){
+		
+		$("#loginform").bootstrapValidator({
+			fields: {
+	        	userName : {validators: {notEmpty : {}}},
+	        	password : {validators: {notEmpty : {}}}
+	        }
+		});
+		
+		var options = {
+			url: getProjectName()+"/login/login.do",
+			type: 'get',
+			success: function(result){
+				$("#loginform .alert").remove();
+				if (result == 1) {
+					window.location.href = getProjectName() + "/page/frame.html";
+				} else if (result == 0){
+					$("#loginform").append('<div class="alert alert-warning" role="alert" style="margin-top:10px">不存在该用户</div>');
+				} else{
+					$("#loginform").append('<div class="alert alert-warning" role="alert" style="margin-top:10px">用户名密码不正确</div>');
+				}
+			}
+		};
+		
+		$("#btn_login").click(function(){
+			$("#loginform").bootstrapValidator('validate');
+			if (!$("#loginform").data('bootstrapValidator').isValid()) {	        
+				return;
+			}
+			$("#loginform").ajaxSubmit(options);
+		});
+	}
+
+	
+	
+	return {
+        login: function () {
+        	initLoginModule();
+        },
+
+        main: function () {
+        	handleSidebar("mtl/category.html"); 
+    		nthTabs = $("#editor-tabs").nthTabs();
+            nthTabs.addTab({
+                id:'0101',
+                title:'原材料.品类',
+                active:true,
+                allowClose:false,
+                content:'mtl/category.html',
+            });
+            
+            handleChangePWD();
+			handleSignOut();
+			handleSidebarCollapse();
+        },
+
+    };
+}();
+
+
+
+var getProjectName = function(){
+	var curWwwPath=window.document.location.href;
+	var pathName=window.document.location.pathname;
+    var pos=curWwwPath.indexOf(pathName);
+	var localhostPaht=curWwwPath.substring(0,pos);
+	var projectName=pathName.substring(0,pathName.substr(1).indexOf('/')+1);
+	return localhostPaht+projectName;
+}
+
+var exchangeWhenInput = function(first, second){
+	first.on('input', function(){
+		second.val($(this).val());
+	});
+	second.on('input', function(){
+		first.val($(this).val());
+	});
+}
+
+function isNumber(value) {  
+    var patrn = /^(-)?\d+(\.\d+)?$/;
+    if (patrn.exec(value) == null || value == "") {
+        return false
+    } else {
+        return true
+    }
+}
+
+
+
